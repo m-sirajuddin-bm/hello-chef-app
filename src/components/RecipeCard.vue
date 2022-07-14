0fcf218a50d62dc1fb2ad7a9d291ad06edbf2d1d
m-sirajuddin-bm <m.sirajuddin.bm@gmail.com>
2022-07-15 04:31:40 +0530

    :sparkles: feat(recipecard): updated the UI based on the design

--- src/components/RecipeCard.vue
+++ src/components/RecipeCard.vue
@@ -1,4 +1,14 @@
 <script setup>
+import CalorieSmartIcon from "@/assets/icons/calorie_smart.svg";
+import ChickenIcon from "@/assets/icons/chicken.svg";
+import FamilyFriendlyIcon from "@/assets/icons/family_friendly.svg";
+import FishIcon from "@/assets/icons/fish.svg";
+import GourmetIcon from "@/assets/icons/gourmet.svg";
+import LowCarbIcon from "@/assets/icons/low_carb.svg";
+import MeatIcon from "@/assets/icons/meat.svg";
+import QuickEasyIcon from "@/assets/icons/quick_easy.svg";
+import VeganIcon from "@/assets/icons/vegan.svg";
+import VegetarianIcon from "@/assets/icons/vegetarian.svg";
 import { ref, toRefs } from "vue";
 
 const props = defineProps({
@@ -11,7 +21,8 @@ const props = defineProps({
 const { product } = toRefs(props);
 const features = ref([]);
 const imageUrl = ref("");
-const featureColors = ref({
+
+const labelColors = {
   "Low Carb": "text-orange-500",
   "Family Friendly": "text-blue-500",
   Vegetarian: "text-green-500",
@@ -19,7 +30,26 @@ const featureColors = ref({
   "Quick & Easy": "text-red-500",
   "Chef's choice": "text-red-500",
   "Calorie smart": "text-purple-500",
-});
+  Fish: "text-red-600",
+  Chicken: "text-red-600",
+  Meat: "text-red-600",
+};
+
+const proteinIcon = {
+  Vegetarian: VegetarianIcon,
+  Vegan: VeganIcon,
+  Fish: FishIcon,
+  Chicken: ChickenIcon,
+  Meat: MeatIcon,
+};
+
+const featuresIcon = {
+  "Low Carb": LowCarbIcon,
+  "Calorie smart": CalorieSmartIcon,
+  "Family Friendly": FamilyFriendlyIcon,
+  "Quick & Easy": QuickEasyIcon,
+  Gourmet: GourmetIcon,
+};
 
 const init = async () => {
   let photo = product.value.photo;
@@ -38,36 +68,69 @@ init();
 </script>
 <template>
   <div
-    class="w-full h-full lg:max-w-sm bg-white aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none"
+    class="aspect-w-1 aspect-h-1 lg:aspect-none flex h-full w-full cursor-pointer flex-row-reverse justify-between gap-2 overflow-hidden border-b bg-white p-4 group-hover:opacity-90 sm:block sm:rounded-xl sm:border sm:p-0 sm:shadow-sm sm:group-hover:border-orange-500 sm:group-hover:shadow-lg lg:max-w-sm"
   >
     <img
       :src="imageUrl"
       :alt="product.cardName1"
-      class="w-full h-52 object-center object-cover"
+      class="h-52 w-48 rounded-xl object-cover object-center shadow-md sm:w-full sm:rounded-none sm:shadow-none"
     />
-    <div class="p-4 flex flex-col h-[calc(100%-208px)] justify-between">
-      <div class="h-20">
-        <h3 class="text-gray-600 font-semibold">{{ product.cardName1 }}</h3>
-        <h3 class="text-gray-400 mt-1 text-sm">{{ product.cardName2 }}</h3>
+    <div
+      class="flex flex-col justify-between pr-2 sm:h-[calc(100%-208px)] sm:p-4"
+    >
+      <div class="sm:h-20">
+        <div class="mb-2 flex">
+          <img
+            :src="proteinIcon[product.mainProtein.name]"
+            class="mr-1 h-5 w-5 rounded text-gray-500"
+            alt=""
+          />
+          <h3
+            :class="[
+              'text-sm font-semibold text-gray-600',
+              labelColors[product.mainProtein.name],
+            ]"
+          >
+            {{ product.mainProtein.name.toUpperCase() }}
+          </h3>
+        </div>
+        <h3 class="font-semibold text-gray-600">{{ product.cardName1 }}</h3>
+        <h3 class="mt-1 text-sm text-gray-400">
+          {{ product.cardName2 }}
+        </h3>
       </div>
-      <div class="flex flex-col gap-2 h-20 justify-end">
-        <div class="flex gap-2 text-gray-600 text-sm">
+      <div class="flex flex-col justify-end gap-2 sm:h-20">
+        <div class="flex gap-2 text-sm text-gray-600">
           <span>{{ product.nutritions[0].value }} Cals</span>
           <span>|</span>
           <span>{{ product.cookingTime }} Min</span>
         </div>
-        <div class="flex text-gray-600 text-sm">
-          <span
-            v-for="(feature, i) in features"
-            :key="i"
-            :class="[featureColors[feature], 'truncate']"
-            >{{ feature
-            }}<span
-              v-if="features.length - 1 !== i"
-              class="text-gray-700 mx-0.5 font-semibold"
-              >&middot;</span
-            ></span
-          >
+        <div class="flex text-sm text-gray-600">
+          <div v-for="(feature, i) in features" :key="i">
+            <div v-if="featuresIcon[feature]" class="flex">
+              <img
+                :src="featuresIcon[feature]"
+                class="h-5 w-5 rounded text-gray-500"
+                alt=""
+              />
+
+              <div v-if="features.length - 1 !== i" class="mx-1 text-gray-700">
+                |
+              </div>
+            </div>
+
+            <!-- <div
+              v-if="!featuresIcon[feature]"
+              :class="[labelColors[feature], 'truncate']"
+            >
+              {{ feature }}
+              <span
+                v-if="features.length - 1 !== i"
+                class="text-gray-700 mx-0.5 font-semibold"
+                >&middot;
+              </span>
+            </div> -->
+          </div>
         </div>
       </div>
     </div>
