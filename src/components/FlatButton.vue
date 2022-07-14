89cc8c98a9e1d33a0cc6dfcb1ca0481e0a580f32
m-sirajuddin-bm <m.sirajuddin.bm@gmail.com>
2022-07-15 04:29:26 +0530

    :sparkles: feat(button): added new common flat button

---
+++ src/components/FlatButton.vue
@@ -0,0 +1,31 @@
+<script setup>
+const emits = defineEmits(["onClick"]);
+
+defineProps({
+  selected: {
+    type: Boolean,
+    required: false,
+  },
+  disabled: {
+    type: Boolean,
+    required: false,
+  },
+});
+</script>
+<template>
+  <button
+    type="button"
+    :disabled="disabled"
+    :class="[
+      'inline-flex items-center rounded-lg border border-gray-300 bg-white px-3 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50',
+      selected
+        ? 'border-orange-500 bg-orange-500/10 hover:bg-orange-500/10'
+        : '',
+      disabled ? 'border-dashed text-gray-400/90 line-through' : '',
+    ]"
+    :style="disabled ? 'filter: grayscale(100%)' : ''"
+    @click="() => emits('onClick')"
+  >
+    <slot></slot>
+  </button>
+</template>
