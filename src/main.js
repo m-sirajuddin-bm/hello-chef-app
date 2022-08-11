2a1dccebab96e6222975efca20080d9bed0e7561
m-sirajuddin-bm <m.sirajuddin.bm@gmail.com>
2022-08-11 23:18:21 +0530

    :sparkles: feat(polyfill): added new date formatter polyfill

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+import "./polyfills/dateFormatter";
 import { createPinia } from "pinia";
 
 const app = createApp(App);
